f5e14a885b805860a6d8cc6cca7fb5f30ac3438a
Mohammad Zaid <mdzaid786291@gmail.com>
2024-09-02 22:40:26 +0530

    checking things

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-
+cap = cv2.VideoCapture("videos/vid1.mp4")
+new_width = 600
+new_height = 600
 while True:
     ret, frame = cap.read()
-
-    cv2.imshow("window", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    frame = cv2.resize(frame, (new_width,new_height))
+    if not ret:
         break
+    else:
+        cv2.imshow("window", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 cap.release()
 cv2.destroyAllWindows()
