5212230a0d9fe19cb96ed26e0e6a936317c5ebde
Mohammad Zaid <mdzaid786291@gmail.com>
2024-10-01 23:27:25 +0530

    added contours detection

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ while True:
     gaussian = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(gray, 150, 100)
     ret, thresh = cv2.threshold(canny, 100, 255, cv2.THRESH_BINARY)
+    contour, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
+    for cnt in contour:
+        print(cv2.contourArea(cnt))
+        if cv2.contourArea(cnt) > 264:
+            cv2.drawContours(frame, cnt, -1, (0, 255, 0), 3)
     if not ret:
         break
     else:
-        cv2.imshow("window", thresh)
+        cv2.imshow("window", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 cap.release()
