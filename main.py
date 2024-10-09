7c5743a96ba62621366927c7ed40aa846eb9df91
Mohammad Zaid <mdzaid786291@gmail.com>
2024-10-09 14:28:59 +0530

    Nothing ;

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
     if not ret:
         break
     else:
-        cv2.imshow("window", frame)
+        cv2.imshow("window", thresh)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 cap.release()
